4021776311f3a6877f78b900375d8c9541ff4e8c
Trusimo <simonetrunfio96@gmail.com>
2022-06-13 15:49:19 +0200

    adds objects and css

--- main.js
+++ main.js
@@ -2,9 +2,24 @@ const appVue = new Vue({
 
     el: "#app",
 
-    data: {
-
-},
+    data: (
+        {
+            text: "Seguire lezione",
+            done: false
+        },
+        {
+            text: "Andare in palestra",
+            done: false
+        },
+        {
+            text: "Fare l'esercizio",
+            done: false
+        },
+        {
+            text: "Andare a calcetto",
+            done: false
+        }
+),
 	methods: {
 
 	}
